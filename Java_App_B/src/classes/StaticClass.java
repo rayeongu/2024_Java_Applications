db7573eb0088bc8d2ee35c508dfa01e1e618b404
rayeongu <rayeongu@gmail.com>
2025-05-26 14:32:58 +0900

    (클래스)4 static 이해하기

---
+++ Java_App_B/src/classes/StaticClass.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class StaticClass {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		StaticClass sc = new StaticClass();
+		int result = sc.add(a, b);
+		System.out.println(result);
+		
+	}
+
+	private int add(int a, int b) {
+		return a + b;
+	}
+}
