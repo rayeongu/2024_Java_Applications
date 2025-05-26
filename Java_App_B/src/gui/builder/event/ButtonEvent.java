341111c64445e74da7fd702dd7d76e77927e699f
rayeongu <rayeongu@gmail.com>
2025-05-26 15:44:20 +0900

    (GUI)12 버튼 이벤트 기본 설정

---
+++ Java_App_B/src/gui/builder/event/ButtonEvent.java
@@ -0,0 +1,62 @@
+package gui.builder.event;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ButtonEvent {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ButtonEvent window = new ButtonEvent();
+					window.frame.setLocationRelativeTo(null);
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public ButtonEvent() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("버튼 이벤트");
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btnAction = new JButton("Action");
+		btnAction.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("클릭");
+			}
+		});
+		btnAction.setFont(new Font("돋움", Font.BOLD, 20));
+		btnAction.setBounds(172, 5, 200, 100);
+		frame.getContentPane().add(btnAction);
+	}
+
+}
