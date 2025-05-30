ccfd453c75e81f4c826776d518cb5751b56bdea4
rayeongu <rayeongu@gmail.com>
2025-05-30 17:36:57 +0900

    (GUI)33 단순계산기 디자인하기

---
+++ Java_App_B/src/gui/builder/app/SimpleCalc.java
@@ -0,0 +1,98 @@
+package gui.builder.app;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+public class SimpleCalc {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					SimpleCalc window = new SimpleCalc();
+					window.frame.setLocationRelativeTo(null);
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SimpleCalc() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("간단한 계산기");
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("X = ");
+		lblNewLabel.setFont(new Font("굴림", Font.BOLD, 20));
+		lblNewLabel.setBounds(26, 45, 50, 15);
+		frame.getContentPane().add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(104, 42, 96, 21);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblY = new JLabel("Y = ");
+		lblY.setFont(new Font("굴림", Font.BOLD, 20));
+		lblY.setBounds(228, 45, 50, 15);
+		frame.getContentPane().add(lblY);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(306, 42, 96, 21);
+		frame.getContentPane().add(textField_1);
+		
+		JButton btnNewButton = new JButton("+");
+		btnNewButton.setFont(new Font("굴림", Font.BOLD, 20));
+		btnNewButton.setBounds(71, 70, 50, 39);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("-");
+		btnNewButton_1.setFont(new Font("굴림", Font.BOLD, 20));
+		btnNewButton_1.setBounds(192, 70, 50, 39);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JButton btnNewButton_1_1 = new JButton("C");
+		btnNewButton_1_1.setFont(new Font("굴림", Font.BOLD, 20));
+		btnNewButton_1_1.setBounds(313, 70, 50, 39);
+		frame.getContentPane().add(btnNewButton_1_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(218, 122, 96, 34);
+		frame.getContentPane().add(textField_2);
+		
+		JLabel lblResult = new JLabel("Result =");
+		lblResult.setFont(new Font("굴림", Font.BOLD, 20));
+		lblResult.setBounds(109, 124, 81, 31);
+		frame.getContentPane().add(lblResult);
+	}
+}
